f041cb1d6a61310904be28f01d64225f6437839f
Van Darrell Ponce <riversideriver@yahoo.com>
2021-07-20 15:18:44 +0800

    reminder on event types

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -1,8 +1,16 @@
+import { useState } from 'react'
+
 const RepositoryList: React.FC = () => {
+  const [term, setTerm] = useState<string>('')
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+  }
+
   return (
     <div>
-      <form>
-        <input />
+      <form onSubmit={onSubmit}>
+        <input value={term} onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
     </div>
