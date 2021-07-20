efe7711858c4efdc8470dc0159bed91f0112f584
Van Darrell Ponce <riversideriver@yahoo.com>
2021-07-20 15:27:58 +0800

    calling an action creator

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -1,10 +1,15 @@
 import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { actionCreators } from '../state'
 
 const RepositoryList: React.FC = () => {
   const [term, setTerm] = useState<string>('')
 
+  const dispatch = useDispatch()
+
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
+    dispatch(actionCreators.searchRepositories(term))
   }
 
   return (
