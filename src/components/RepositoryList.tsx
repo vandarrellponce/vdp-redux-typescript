c4f701a80ea67949922a813c7f6829f11e917d58
Van Darrell Ponce <riversideriver@yahoo.com>
2021-07-20 15:10:05 +0800

    wiring up to react

---
+++ src/components/RepositoryList.tsx
@@ -0,0 +1,12 @@
+const RepositoryList: React.FC = () => {
+  return (
+    <div>
+      <form>
+        <input />
+        <button>Search</button>
+      </form>
+    </div>
+  )
+}
+
+export default RepositoryList
