c26e794e3792ff65f8f8db3961ea93cdd33e524d
Van Darrell Ponce <riversideriver@yahoo.com>
2021-07-21 11:37:11 +0800

    awkward typings around react-redux

--- src/state/reducers/index.ts
+++ src/state/reducers/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import repositoryReducer from './repoReducer'
 
-const reducers = combineReducers({
+const rootReducer = combineReducers({
   repositories: repositoryReducer,
 })
 
-export default reducers
+export default rootReducer
+
+export type RootState = ReturnType<typeof rootReducer>
