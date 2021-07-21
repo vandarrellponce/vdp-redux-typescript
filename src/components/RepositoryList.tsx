772f8f76208aa4d0df377187f10119c4407e5554
Van Darrell Ponce <riversideriver@yahoo.com>
2021-07-21 12:10:18 +0800

    consuming store state

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -20,6 +20,9 @@ const RepositoryList: React.FC = () => {
         <input value={term} onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && data.map((name, i) => <p key={i}>{name}</p>)}
     </div>
   )
 }
